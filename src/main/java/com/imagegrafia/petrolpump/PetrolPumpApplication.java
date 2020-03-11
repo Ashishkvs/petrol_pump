c576a642700bdbda409b1b617cdad54e07e730c2
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2020-03-11 09:25:52 +0530

    tomcat config to deploy

--- src/main/java/com/imagegrafia/petrolpump/PetrolPumpApplication.java
+++ src/main/java/com/imagegrafia/petrolpump/PetrolPumpApplication.java
@@ -2,9 +2,17 @@ package com.imagegrafia.petrolpump;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetrolPumpApplication {
+public class PetrolPumpApplication extends SpringBootServletInitializer{
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PetrolPumpApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetrolPumpApplication.class, args);
